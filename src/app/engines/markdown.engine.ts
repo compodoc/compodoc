901d58c77ba1f773a48a7149837a3fe4aa7a1b7a
vogloblinsky <vincent.ogloblinsky@gmail.com>
2016-10-19 14:40:54 +0200

    fix(configuration): syntax

--- src/app/engines/markdown.engine.ts
+++ src/app/engines/markdown.engine.ts
@@ -10,7 +10,7 @@ export class MarkdownEngine {
         return new Promise(function(resolve, reject) {
            fs.readFile(path.resolve(process.cwd() + '/README.md'), 'utf8', (err, data) => {
                if (err) {
-                   reject('Error during README reading');
+                   reject('Error during README.md file reading');
                } else {
                    resolve(marked(data));
                }
