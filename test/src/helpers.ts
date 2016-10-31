bef196de75ace75db99e82fad6511f0caff1800a
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2016-10-31 23:35:00 +0100

    feat(tests): tests CLI arguments

---
+++ test/src/helpers.ts
@@ -0,0 +1,42 @@
+export const shell = require('child_process').spawnSync;
+export const shellAsync = require('child_process').spawn;
+export const fs = require('fs');
+export const path = require('path');
+export const pkg = require('../../package.json');
+
+export function read(file: string): string {
+    return fs.readFileSync(file).toString();
+}
+
+export function exists(file: string): boolean {
+    return fs.existsSync(file);
+}
+
+export function temporaryDir() {
+    let name = '.tmp-compodoc-test';
+    let cleanUp = (name) => {
+        if( fs.existsSync(name) ) {
+            fs.readdirSync(name).forEach((file) => {
+                var curdir = path.join(name, file);
+                if(fs.statSync(curdir).isDirectory()) {
+                    cleanUp(curdir);
+                } else {
+                    fs.unlinkSync(curdir);
+                }
+            });
+            fs.rmdirSync(name);
+        }
+    };
+
+    return {
+        name,
+        create() {
+            if (!fs.existsSync(name)){
+                fs.mkdirSync(name);
+            }
+        },
+        clean() {
+            cleanUp(name);
+        }
+    }
+}
