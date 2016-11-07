b7ab5942592e1c54e540fe6817f962b688560ed3
vogloblinsky <vincent.ogloblinsky@gmail.com>
2016-11-07 16:12:43 +0100

    feat(app): mobile menu

--- src/resources/js/menu.js
+++ src/resources/js/menu.js
@@ -7,4 +7,16 @@ document.addEventListener('DOMContentLoaded', function() {
             window.location.href = link;
         })
     })
+
+    var menuCollapsed = false,
+        mobileMenu = document.getElementById('mobile-menu');
+    document.getElementById('btn-menu').addEventListener('click', function() {
+        if (menuCollapsed) {
+            mobileMenu.style.display = 'none';
+        } else {
+            mobileMenu.style.display = 'block';
+            document.getElementsByTagName('body')[0].style['overflow-y'] = 'hidden';
+        }
+        menuCollapsed = !menuCollapsed;
+    })
 });
