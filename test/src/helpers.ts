73de2b2f0113714dc8f42352425f4cc86d6bdd1a
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2016-11-12 23:47:47 +0100

    feat(test): exec shortcut

--- test/src/helpers.ts
+++ test/src/helpers.ts
@@ -1,4 +1,5 @@
 export const shell = require('child_process').spawnSync;
+export const exec = require('child_process').exec;
 export const shellAsync = require('child_process').spawn;
 export const fs = require('fs');
 export const path = require('path');
