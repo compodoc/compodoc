c4800a7a2fa13e243b51a66b288e5914b3cfeba7
vogloblinsky <vincent.ogloblinsky@gmail.com>
2016-11-15 16:04:34 +0100

    feat(app): syntax highlighting for markdowns files fix #19

--- src/app/engines/markdown.engine.ts
+++ src/app/engines/markdown.engine.ts
@@ -1,20 +1,29 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import marked from 'marked';
+import marked, { Renderer } from 'marked';
+import highlightjs from 'highlight.js';
 
 export class MarkdownEngine {
     constructor() {
+        const renderer = new Renderer();
+        renderer.code = (code, language) => {
+            const validLang = !!(language && highlightjs.getLanguage(language));
+            let highlighted = validLang ? highlightjs.highlight(language, code).value : code;
+            highlighted = highlighted.replace(/(\r\n|\n|\r)/gm, '<br>');
+            return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
+        };
 
+        marked.setOptions({ renderer });
     }
     getReadmeFile() {
         return new Promise(function(resolve, reject) {
-           fs.readFile(path.resolve(process.cwd() + '/README.md'), 'utf8', (err, data) => {
-               if (err) {
-                   reject('Error during README.md file reading');
-               } else {
-                   resolve(marked(data));
-               }
-           });
+            fs.readFile(path.resolve(process.cwd() + '/README.md'), 'utf8', (err, data) => {
+                if (err) {
+                    reject('Error during README.md file reading');
+                } else {
+                    resolve(marked(data));
+                }
+            });
         });
     }
 };
