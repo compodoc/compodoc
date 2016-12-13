b7da879f3c5e4f74f5354b89e49ef1fc964d0d81
vogloblinsky <vincent.ogloblinsky@gmail.com>
2016-12-13 17:35:59 +0100

    feat(release): screenshots node script [ci skip]

--- tools/release-screenshots.js
+++ tools/release-screenshots.js
@@ -1,41 +1,90 @@
 const webshot = require('webshot'),
+    process = require('process'),
     liveServer = require('live-server'),
     helpers = require('../test/dist/helpers.js'),
+    rimraf = require('rimraf'),
     exec = helpers.exec,
     fs = helpers.fs,
-    tmp = helpers.temporaryDir();
+    tmp = helpers.temporaryDir(),
 
-/*
+    THEMES = ['gitbook', 'laravel', 'original', 'postmark', 'readthedocs', 'stripe', 'vagrant'],
+
+    len = THEMES.length,
+
+    SHOT_OPTIONS = {
+        windowSize: {
+            width: 1024,
+            height: 768
+        },
+        customCSS: '.menu{overflow-y:hidden!important;}'
+    },
+
+    shot = function(file) {
+        return new Promise(function(resolve, reject) {
+            webshot('http://localhost:8080', file, SHOT_OPTIONS, function(err) {
+                console.log('todomvc-ng2 screenshot generated : ', file);
+                resolve();
+            });
+        });
+    },
+
+    document = function(theme) {
+        console.log('Document with ' + theme);
+        var command = 'node ../bin/index.js -p ./src/tsconfig.json -n \'TodoMVC Angular 2 documentation\' ';
+        if (theme !== 'gitbook') {
+            command += ' -h ' + theme
+        }
+        return new Promise(function(resolve, reject) {
+            exec(command, {
+                env: {
+                    MODE: 'TESTING'
+                }
+            }, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    reject();
+                } else {
+                    shot('../screenshots/theme-' + theme + '.png').then(function() {
+                        resolve();
+                    })
+                }
+            });
+        });
+    };
+
+var i = 0;
+
 tmp.create();
-exec('node ./bin/index.js -p ./test/src/todomvc-ng2/tsconfig.json -n \'TodoMVC Angular 2 documentation\' -d ' + tmp.name, {
-    env: {
-        MODE: 'TESTING'
-    }
-}, (error, stdout, stderr) => {
+exec('cd ' + tmp.name + ' && git clone https://github.com/compodoc/compodoc-demo-todomvc-angular2.git .', {}, (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
     } else {
-        console.log('todomvc-ng2 doc generated');
+        console.log('todomvc-ng2 git clone');
+        process.chdir(tmp.name);
 
-        /*
         liveServer.start({
-            root: tmp.name,
+            root: './documentation',
             open: false,
             quiet: true,
             logLevel: 0
         });
-        */
-        /*
-        fs.readFile(tmp.name + '/index.html', 'utf8', (err, data) => {
-            if (err) throw err;
-            webshot(data, 'compodoc.png', {
-                siteType: 'html'
-            }, function(err) {
-                console.log('todomvc-ng2 screenshot generated');
-            });
-        });
-        */
-        /*
+
+        let loop = function() {
+            if (i < len) {
+                document(THEMES[i]).then(function() {
+                    i++
+                    loop();
+                })
+                .catch((error) => {
+                    console.log('document error: ', error);
+                });
+            } else {
+                console.log('END');
+                liveServer.shutdown();
+                process.exit(0);
+                rimraf(tmp.name);
+            }
+        };
+        loop();
     }
 });
-*/
