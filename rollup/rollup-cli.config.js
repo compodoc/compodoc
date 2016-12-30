e9e5b5d62aff0e73d8f8eca658f9e6242db16123
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2016-12-30 14:57:13 +0100

    feat(app): update rollup config files

--- rollup/rollup-cli.config.js
+++ rollup/rollup-cli.config.js
@@ -3,13 +3,25 @@ import typescript from 'rollup-plugin-typescript';
 import babel from 'rollup-plugin-babel';
 
 export default {
-    entry: './src/index-cli.ts',
-    dest: 'dist/index-cli.js',
+    entry: './src/index.ts',
+    dest: 'dist/index.js',
     format: 'cjs',
     plugins: [
         typescript({
             typescript: require('typescript')
         }),
         babel()
+    ],
+    external: [
+        'handlebars',
+        'marked',
+        'lodash',
+        'path',
+        'util',
+        'fs-extra',
+        'live-server',
+        'shelljs',
+        'typescript',
+        'highlight.js'
     ]
 }
