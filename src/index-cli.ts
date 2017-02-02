4ebbdf89a6061625e192779cb708395a90f38e34
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-02-02 16:13:12 +0100

    fix(app): (node:17604) MaxListenersExceededWarning: Possible EventEmitter memory leak detected.

--- src/index-cli.ts
+++ src/index-cli.ts
@@ -11,7 +11,7 @@ let pkg = require('../package.json'),
     files = [],
     cwd = process.cwd();
 
-require('events').EventEmitter.prototype._maxListeners = 100;
+process.setMaxListeners(0);
 
 export class CliApplication extends Application
 {
