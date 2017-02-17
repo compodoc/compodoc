6a335b817aa0946cc1f4051f32facb25d2e749f5
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-02-17 17:03:47 +0100

    feat(app): jsdoc tests
    [skip ci]

--- test/src/sample-files/foo.service.ts
+++ test/src/sample-files/foo.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@angular/core';
 
 @Injectable()
-export class FooService { }
+export class FooService {
+
+    /**
+     * @param {string} val The entry value
+     * @returns {string} The string
+     * @example
+     * FooService.open('yala');
+     */
+    open(val: string): string {
+        return 'test';
+    }
+}
