2dd0fdd32327bd7d783027a948e4686d483527a3
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-02-21 15:10:01 +0100

    feat(app): exclude tests

--- test/src/cli/cli.spec.ts
+++ test/src/cli/cli.spec.ts
@@ -7,7 +7,7 @@ const tsconfigPath = require.resolve('../../../tsconfig.json');
 const tsNodePath = require.resolve('../../../node_modules/.bin/ts-node');
 const env = Object.freeze({TS_NODE_PROJECT: tsconfigPath, MODE:'TESTING'});
 
-describe('CLI simple tags', () => {
+describe('CLI simple flags', () => {
 
     describe('when no tsconfig.json provided', () => {
 
@@ -198,6 +198,28 @@ describe('CLI simple tags', () => {
             expect(moduleFile).not.to.contain('components/BarComponent.html');
             expect(moduleFile).not.to.contain('injectables/FooService.html');
             expect(moduleFile).not.to.contain('modules/BarModule.html');
+        });
+    });
+
+    describe('when specific files are excluded in tsconfig', () => {
+        let stdoutString = null;
+        before(function (done) {
+            tmp.create();
+            exec(tsNodePath + ' ./bin/index-cli.js -p ./test/src/sample-files/tsconfig.exclude.json -d ' + tmp.name + '/', {env}, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`exec error: ${error}`);
+                    done('error');
+                    return;
+                }
+                stdoutString = stdout;
+                done();
+            });
+        });
+        after(() => tmp.clean(tmp.name));
+
+        it('should not create files excluded', () => {
+            const isFileExists = exists(`${tmp.name}/components/BarComponent.js`);
+            expect(isFileExists).to.be.false;
         });
     });
 
