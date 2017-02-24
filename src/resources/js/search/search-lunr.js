d30ad3dfe48646452db2fc93d4472ee03c4c7b07
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-02-24 14:49:51 +0100

    fix(app): reload with querystring from search

--- src/resources/js/search/search-lunr.js
+++ src/resources/js/search/search-lunr.js
@@ -44,18 +44,8 @@
     };
 
     compodoc.addEventListener(compodoc.EVENTS.READY, function(event) {
-        console.log('compodoc ready');
-
         var engine = new LunrSearchEngine(),
             initialized = false;
-
-        engine.init()
-        .then(function() {
-            initialized = true;
-            compodoc.dispatchEvent({
-                type: compodoc.EVENTS.SEARCH_READY
-            });
-        });
 
         function query(q, offset, length) {
             if (!initialized) throw new Error('Search has not been initialized');
@@ -65,5 +55,13 @@
         compodoc.search = {
             query: query
         };
+
+        engine.init()
+        .then(function() {
+            initialized = true;
+            compodoc.dispatchEvent({
+                type: compodoc.EVENTS.SEARCH_READY
+            });
+        });
     });
 })(compodoc);
