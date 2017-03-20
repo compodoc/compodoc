09ce146e85be610c0104365efac6fae79bd67c5d
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-03-20 15:21:52 +0100

    fix(test): cleaning

--- wdio.conf.local.js
+++
@@ -1,9 +0,0 @@
-const base = require('./wdio.conf.base')
-
-exports.config = Object.assign({}, base.config, {
-    capabilities: [{
-            browserName: 'phantomjs'
-        }
-    ],
-    services: ['phantomjs']
-});
