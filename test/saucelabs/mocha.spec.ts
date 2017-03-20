ee32681aca99e658873c333fe66601faa6fd0391
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-03-20 17:12:25 +0100

    feat(test): local e2e test

--- test/saucelabs/mocha.spec.ts
+++ test/saucelabs/mocha.spec.ts
@@ -1,46 +1,56 @@
 var expect = require('chai').expect,
-    test = require('selenium-webdriver/testing'),
     webdriver = require('selenium-webdriver'),
+    test = require('selenium-webdriver/testing'),
     username = process.env.SAUCE_USERNAME,
     accessKey = process.env.SAUCE_ACCESS_KEY,
     capabilities:any = {
-        'platform': 'WIN7',
-        'username': username,
-        'accessKey': accessKey,
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-        name: 'Compodoc test',
-        'public': true,
-        build: process.env.TRAVIS_BUILD_NUMBER
+        'platform': 'WIN7'
     },
-
+    server = '',
     testCode = function() {
-        var driver = new webdriver.Builder()
-            .withCapabilities(capabilities)
-            .usingServer("http://" + username + ":" + accessKey + "@ondemand.saucelabs.com:80/wd/hub")
-            .build();
-        driver.get('http://127.0.0.1:8383/components/FooComponent.html');
+        if (process.env.MODE_LOCAL === '0') {
+            capabilities.username = username;
+            capabilities.accessKey = accessKey;
+            capabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
+            capabilities.name = 'Compodoc test';
+            capabilities.public = true;
+            capabilities.build = process.env.TRAVIS_BUILD_NUMBER;
+            server = 'http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub';
+        }
+        if (process.env.MODE_LOCAL === '1') {
+            capabilities.platform = 'Linux';
+            server = 'http://localhost:4444/wd/hub';
+        }
+
+        driver = new webdriver.Builder()
+        .withCapabilities(capabilities)
+        .usingServer(server)
+        .build();
+
+        driver.get('http://127.0.0.1:8686/components/FooComponent.html');
 
         driver.getTitle().then(function(title) {
             expect(title).to.equal('@compodoc/compodoc documentation');
         });
 
         driver.quit();
-    };
+    },
+    driver;
 
 // Chrome
 test.describe('Chrome | Compodoc page', function() {
     test.it('should display title', function() {
         capabilities.browserName = 'chrome';
-        capabilities.version = '56';
+        capabilities.version = '57';
         testCode();
     });
 });
 
-// Chrome
+// Firefox
 test.describe('Firefox | Compodoc page', function() {
     test.it('should display title', function() {
         capabilities.browserName = 'firefox';
-        capabilities.version = '51';
+        capabilities.version = '52';
         testCode();
     });
 });
