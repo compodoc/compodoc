3d3cd311f325fc22c3a544c068301aa876d9ae48
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-03-21 16:49:00 +0100

    feat(test): e2e tests for search bar

--- test/saucelabs/mocha.spec.ts
+++ test/saucelabs/mocha.spec.ts
@@ -1,14 +1,14 @@
 // RUN webdriver-manager start --standalone & npm run test:simple-doc before starting local test
 var expect = require('chai').expect,
+    fs = require('fs'),
     webdriver = require('selenium-webdriver'),
-    test = require('selenium-webdriver/testing'),
     username = process.env.SAUCE_USERNAME,
     accessKey = process.env.SAUCE_ACCESS_KEY,
-    capabilities:any = {
+    capabilities: any = {
         'platform': 'WIN7'
     },
     server = '',
-    testCode = function() {
+    startDriver = function(cb, pageUrl) {
         if (process.env.MODE_LOCAL === '0') {
             capabilities.username = username;
             capabilities.accessKey = accessKey;
@@ -23,37 +23,115 @@ var expect = require('chai').expect,
             server = 'http://localhost:4444/wd/hub';
         }
 
-        console.log(capabilities, server);
-
         driver = new webdriver.Builder()
-        .withCapabilities(capabilities)
-        .usingServer(server)
-        .build();
+            .withCapabilities(capabilities)
+            .usingServer(server)
+            .build();
 
-        driver.get('http://127.0.0.1:8383/components/FooComponent.html');
-
-        driver.getTitle().then(function(title) {
-            expect(title).to.equal('@compodoc/compodoc documentation');
+        driver.get(pageUrl).then(function() {
+            cb();
         });
-
-        driver.quit();
+    },
+    writeScreenshot = function(data, name) {
+        fs.writeFile('out.png', data, 'base64', function(err) {
+            if (err) console.log(err);
+        });
     },
     driver;
 
 // Chrome
-test.describe('Chrome | Compodoc page', function() {
-    test.it('should display title', function() {
+describe('Chrome | Compodoc page', function() {
+
+    before(function(done) {
         capabilities.browserName = 'chrome';
         capabilities.version = '56';
-        testCode();
+
+        startDriver(done, 'http://127.0.0.1:8383/components/FooComponent.html');
+    });
+
+    it('should display title', function(done) {
+        driver.getTitle().then(function(title) {
+            expect(title).to.equal('@compodoc/compodoc documentation');
+            done();
+        });
+    });
+
+    // test search bar
+
+    it('should have a search bar, and handle results', function(done) {
+        var searchBox
+        driver
+            .findElements(webdriver.By.xpath("//div[@id='book-search-input']/input"))
+            .then(function(elems) {
+                searchBox = elems[1]; //First one is the mobile one hidden;
+                searchBox.sendKeys('exampleInput');
+                searchBox.getAttribute('value').then(function(value) {
+                    expect(value).to.equal('exampleInput');
+
+                    /*driver.takeScreenshot().then(function (data) {
+                        writeScreenshot(data, 'test.png');
+                    });*/
+
+                    driver
+                        .findElements(webdriver.By.className('search-results-item'))
+                        .then(function(elems) {
+                            expect(elems.length).to.equal(1);
+                            done();
+                        });
+                });
+            });
+    });
+
+    it('should have a search bar, and handle results empty', function(done) {
+        var searchBox
+        driver
+            .findElements(webdriver.By.xpath("//div[@id='book-search-input']/input"))
+            .then(function(elems) {
+                searchBox = elems[1]; //First one is the mobile one hidden;
+                searchBox.clear();
+                searchBox.sendKeys('waza');
+                searchBox.getAttribute('value').then(function(value) {
+                    expect(value).to.equal('waza');
+
+                    driver
+                        .findElements(webdriver.By.className('search-results-item'))
+                        .then(function(elems) {
+                            expect(elems.length).to.equal(0);
+                            done();
+                        });
+                });
+            });
+    });
+
+    // test click sur DOM tree tab, et generation du canvas
+
+    // test routing
+
+    after(function(done) {
+        // works with promise
+        driver.quit().then(done);
     });
 });
 
 // Firefox
-test.describe('Firefox | Compodoc page', function() {
-    test.it('should display title', function() {
+describe('Firefox | Compodoc page', function() {
+
+    before(function(done) {
         capabilities.browserName = 'firefox';
         capabilities.version = '51';
-        testCode();
+
+        startDriver(done, 'http://127.0.0.1:8383/components/FooComponent.html');
+    });
+
+    it('should display title', function(done) {
+        driver.getTitle().then(function(title) {
+            expect(title).to.equal('@compodoc/compodoc documentation');
+            done();
+        });
+    });
+
+    after(function(done) {
+        // works with promise
+        driver.quit().then(done);
     });
 });
