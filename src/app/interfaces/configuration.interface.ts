8ad6941459cf360efc79ccc125a3cb827666250a
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-05-03 16:58:41 +0200

    fix(tsc): errors

---
+++ src/app/interfaces/configuration.interface.ts
@@ -0,0 +1,9 @@
+import { MainDataInterface } from './main-data.interface';
+import { PageInterface } from './page.interface';
+
+export interface ConfigurationInterface {
+    mainData: MainDataInterface;
+    pages:PageInterface[];
+    addPage(page: PageInterface): void;
+    addAdditionalPage(page: PageInterface): void;
+}
