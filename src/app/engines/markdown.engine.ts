e816cbea60d8ef556d8735c67c0e242b6e6ce830
Thomas Mair <thomas.mair@jambit.com>
2017-05-09 13:06:52 +0200

    Fix escaping of html entities in code blocks

--- src/app/engines/markdown.engine.ts
+++ src/app/engines/markdown.engine.ts
@@ -10,6 +10,8 @@ export class MarkdownEngine {
             if (!language) {
                 language = 'none';
             }
+
+            highlighted = this.escape(code);
             return `<pre class="line-numbers"><code class="language-${language}">${highlighted}</code></pre>`;
         };
 
@@ -24,13 +26,13 @@ export class MarkdownEngine {
                 + '</table>\n';
         }
 
-        renderer.image = function(href, title, text) {
-          var out = '<img src="' + href + '" alt="' + text + '" class="img-responsive"';
-          if (title) {
-            out += ' title="' + title + '"';
-          }
-          out += this.options.xhtml ? '/>' : '>';
-          return out;
+        renderer.image = function (href, title, text) {
+            var out = '<img src="' + href + '" alt="' + text + '" class="img-responsive"';
+            if (title) {
+                out += ' title="' + title + '"';
+            }
+            out += this.options.xhtml ? '/>' : '>';
+            return out;
         };
 
         marked.setOptions({
@@ -38,19 +40,19 @@ export class MarkdownEngine {
             breaks: true
         });
     }
-    get(filepath:string) {
-        return new Promise(function(resolve, reject) {
-           fs.readFile(path.resolve(process.cwd() + path.sep + filepath), 'utf8', (err, data) => {
-               if (err) {
-                   reject('Error during ' + filepath + ' read');
-               } else {
-                   resolve(marked(data));
-               }
-           });
+    get(filepath: string) {
+        return new Promise(function (resolve, reject) {
+            fs.readFile(path.resolve(process.cwd() + path.sep + filepath), 'utf8', (err, data) => {
+                if (err) {
+                    reject('Error during ' + filepath + ' read');
+                } else {
+                    resolve(marked(data));
+                }
+            });
         });
     }
     getReadmeFile() {
-        return new Promise(function(resolve, reject) {
+        return new Promise(function (resolve, reject) {
             fs.readFile(path.resolve(process.cwd() + '/README.md'), 'utf8', (err, data) => {
                 if (err) {
                     reject('Error during README.md file reading');
@@ -77,5 +79,15 @@ export class MarkdownEngine {
             finalPath = readmeAlternativeFile;
         }
         return finalPath;
+    }
+
+    private escape(html) {
+        return html
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/@/g, '&#40;');
     }
 };
