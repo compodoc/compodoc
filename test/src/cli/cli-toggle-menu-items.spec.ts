2cc626bfb1050340aa030010177cf9da3cdacce8
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-07-01 01:21:39 +0200

    fix(tests): Windows cleaning

--- test/src/cli/cli-toggle-menu-items.spec.ts
+++ test/src/cli/cli-toggle-menu-items.spec.ts
@@ -1,11 +1,9 @@
 import * as chai from 'chai';
-const expect = chai.expect;
-
-import {temporaryDir, shell, pkg, exists, exec, read} from '../helpers';
-const tmp = temporaryDir();
-const tsconfigPath = require.resolve('../../../tsconfig.json');
-const tsNodePath = require.resolve('../../../node_modules/.bin/ts-node');
-const env = Object.freeze({TS_NODE_PROJECT: tsconfigPath, MODE:'TESTING'});
+import {temporaryDir, shell, pkg, exists, exec, read, shellAsync} from '../helpers';
+const expect = chai.expect,
+      tmp = temporaryDir(),
+      tsconfigPath = require.resolve('../../../tsconfig.json'),
+      env = Object.freeze({TS_NODE_PROJECT: tsconfigPath, MODE:'TESTING'});
 
 describe('CLI toggle menu items', () => {
 
@@ -15,16 +13,19 @@ describe('CLI toggle menu items', () => {
             fooServiceFile;
         before(function (done) {
             tmp.create();
-            exec(tsNodePath + ' ./bin/index-cli.js -p ./test/src/todomvc-ng2/src/tsconfig.json -d ' + tmp.name + '/ --toggleMenuItems "modules"', {env}, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    done('error');
-                    return;
-                }
-                stdoutString = stdout;
-                fooIndexFile = read(`${tmp.name}/index.html`);
-                done();
-            });
+            let ls = shell('node', [
+                '../bin/index-cli.js',
+                '-p', '../test/src/todomvc-ng2/src/tsconfig.json',
+                '-d', '../' + tmp.name + '/',
+                '--toggleMenuItems', 'modules'], { cwd: tmp.name, env });
+
+            if (ls.stderr.toString() !== '') {
+                console.error(`shell error: ${ls.stderr.toString()}`);
+                done('error');
+            }
+            stdoutString = ls.stdout.toString();
+            fooIndexFile = read(`${tmp.name}/index.html`);
+            done();
         });
         after(() => tmp.clean());
 
