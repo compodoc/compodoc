0ffc80ebac94f01033f649da944b12c589264dc7
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-07-01 01:25:44 +0200

    fix(tests): Windows cleaning

--- test/src/cli/cli-exclude.spec.ts
+++ test/src/cli/cli-exclude.spec.ts
@@ -1,27 +1,25 @@
 import * as chai from 'chai';
-const expect = chai.expect;
-
-import {temporaryDir, shell, pkg, exists, exec, read} from '../helpers';
-const tmp = temporaryDir();
-const tsconfigPath = require.resolve('../../../tsconfig.json');
-const tsNodePath = require.resolve('../../../node_modules/.bin/ts-node');
-const env = Object.freeze({TS_NODE_PROJECT: tsconfigPath, MODE:'TESTING'});
+import {temporaryDir, shell, pkg, exists, exec, read, shellAsync} from '../helpers';
+const expect = chai.expect,
+      tmp = temporaryDir(),
+      tsconfigPath = require.resolve('../../../tsconfig.json'),
+      env = Object.freeze({TS_NODE_PROJECT: tsconfigPath, MODE:'TESTING'});
 
 describe('CLI exclude from tsconfig', () => {
 
     describe('when specific files are excluded in tsconfig', () => {
-        let stdoutString = null;
         before(function (done) {
             tmp.create();
-            exec(tsNodePath + ' ./bin/index-cli.js -p ./test/src/sample-files/tsconfig.exclude.json -d ' + tmp.name + '/', {env}, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    done('error');
-                    return;
-                }
-                stdoutString = stdout;
-                done();
-            });
+            let ls = shell('node', [
+                '../bin/index-cli.js',
+                '-p', '../test/src/sample-files/tsconfig.exclude.json',
+                '-d', '../' + tmp.name + '/'], { cwd: tmp.name, env });
+
+            if (ls.stderr.toString() !== '') {
+                console.error(`shell error: ${ls.stderr.toString()}`);
+                done('error');
+            }
+            done();
         });
         after(() => tmp.clean(tmp.name));
 
