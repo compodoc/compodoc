89f2cfea30b91ebb88f90019d337141cdaac2d87
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2017-07-01 01:08:46 +0200

    fix(tests): Windows cleaning

--- test/src/cli/cli-additional.spec.ts
+++ test/src/cli/cli-additional.spec.ts
@@ -1,11 +1,9 @@
 import * as chai from 'chai';
-const expect = chai.expect;
-
-import {temporaryDir, shell, pkg, exists, exec, read} from '../helpers';
-const tmp = temporaryDir();
-const tsconfigPath = require.resolve('../../../tsconfig.json');
-const tsNodePath = require.resolve('../../../node_modules/.bin/ts-node');
-const env = Object.freeze({TS_NODE_PROJECT: tsconfigPath, MODE:'TESTING'});
+import {temporaryDir, shell, pkg, exists, exec, read, shellAsync} from '../helpers';
+const expect = chai.expect,
+      tmp = temporaryDir(),
+      tsconfigPath = require.resolve('../../../tsconfig.json'),
+      env = Object.freeze({TS_NODE_PROJECT: tsconfigPath, MODE:'TESTING'});
 
 describe('CLI Additional documentation', () => {
 
@@ -14,21 +12,25 @@ describe('CLI Additional documentation', () => {
         fooServiceFile;
     before(function (done) {
         tmp.create();
-        exec(tsNodePath + ' ./bin/index-cli.js -p ./test/src/todomvc-ng2/src/tsconfig.json -d ' + tmp.name + '/ --includes ./test/src/todomvc-ng2/additional-doc --includesName "Additional documentation"', {env}, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                done('error');
-                return;
-            }
-            stdoutString = stdout;
-            fooIndexFile = read(`${tmp.name}/index.html`);
-            done();
-        });
+        let ls = shell('node', [
+            '../bin/index-cli.js',
+            '-p', '../test/src/todomvc-ng2/src/tsconfig.json',
+            '-d', '../' + tmp.name + '/',
+            '--includes', '../test/src/todomvc-ng2/additional-doc',
+            '--includesName', '"Additional documentation"'], { cwd: tmp.name, env });
+
+        if (ls.stderr.toString() !== '') {
+            console.error(`shell error: ${ls.stderr.toString()}`);
+            done('error');
+        }
+        stdoutString = ls.stdout.toString();
+        fooIndexFile = read(`${tmp.name}/index.html`);
+        done();
     });
     after(() => tmp.clean());
 
     it('it should have a menu with links', () => {
-        expect(fooIndexFile).to.contain('<a href="./additional-documentation/introduction');
+        expect(fooIndexFile).to.contain('<a href="additional-documentation/introduction');
     });
     it('it should have generated files', () => {
         const isFileExists = exists(`${tmp.name}/additional-documentation/Edition.html`);
