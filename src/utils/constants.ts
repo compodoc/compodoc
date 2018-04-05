fc2c54f70057e990d7caee525e028e1507039f78
Blake Simmon <blake.simmon@solarwinds.com>
2018-04-05 15:52:42 -0500

    Adjustment for Codacy analysis

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,46 +1,46 @@
 export const COMPODOC_CONSTANTS = {
     navTabDefinitions: [
         {
-            "id": "info",
-            "href": "#info",
-            "data-link": "info",
-            "label": "Info",
-            "depTypes": ['all']
+            'id': 'info',
+            'href': '#info',
+            'data-link': 'info',
+            'label': 'Info',
+            'depTypes': ['all']
         },
         {
-            "id": "readme",
-            "href": "#readme",
-            "data-link": "readme",
-            "label": "README",
-            "depTypes": ['all']
+            'id': 'readme',
+            'href': '#readme',
+            'data-link': 'readme',
+            'label': 'README',
+            'depTypes': ['all']
         },
         {
-            "id": "source",
-            "href": "#source",
-            "data-link": "source",
-            "label": "Source",
-            "depTypes": ['all']
+            'id': 'source',
+            'href': '#source',
+            'data-link': 'source',
+            'label': 'Source',
+            'depTypes': ['all']
         },
         {
-            "id": "templateData",
-            "href": "#templateData",
-            "data-link": "template",
-            "label": "Template",
-            "depTypes": ['component']
+            'id': 'templateData',
+            'href': '#templateData',
+            'data-link': 'template',
+            'label': 'Template',
+            'depTypes': ['component']
         },
         {
-            "id": "tree",
-            "href": "#tree",
-            "data-link": "dom-tree",
-            "label": "DOM Tree",
-            "depTypes": ['component']
+            'id': 'tree',
+            'href': '#tree',
+            'data-link': 'dom-tree',
+            'label': 'DOM Tree',
+            'depTypes': ['component']
         },
         {
-            "id": "example",
-            "href": "#example",
-            "data-link": "example",
-            "label": "Examples",
-            "depTypes": ['component', 'directive', 'injectable', 'pipe']
+            'id': 'example',
+            'href': '#example',
+            'data-link': 'example',
+            'label': 'Examples',
+            'depTypes': ['component', 'directive', 'injectable', 'pipe']
         },
     ]
 };
