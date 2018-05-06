b5e16e8717c729a79b9f36158cea37ecf6a8a9bd
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-05-07 00:54:51 +0200

    fix(UI): back button handling
    fix #557

--- src/resources/js/sourceCode.js
+++ src/resources/js/sourceCode.js
@@ -25,5 +25,29 @@ document.addEventListener('DOMContentLoaded', function() {
                 $content.scrollTop = top;
             }, 500);
         };
+
+        window.onhashchange = function(event) {
+            switch (window.location.hash) {
+                case '':
+                case '#info':
+                    $tabInfo.click();
+                    break;
+                case '#readme':
+                    $tabReadme.click();
+                    break;
+                case '#source':
+                    $tabSource.click();
+                    break;
+                case '#template':
+                    $tabTemplate.click();
+                    break;
+                case '#dom-tree':
+                    $tabTree.click();
+                    break;
+                case '#example':
+                    $tabExample.click();
+                    break;
+            }
+        }
     }
 });
