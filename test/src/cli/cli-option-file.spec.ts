b3e87b65c993307a9c5c612ea209dd6555f5bfac
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-05-15 00:22:00 +0200

    feat(configuration): first tests

---
+++ test/src/cli/cli-option-file.spec.ts
@@ -0,0 +1,52 @@
+import * as chai from 'chai';
+import { temporaryDir, shell, pkg, exists, exec, read, shellAsync } from '../helpers';
+
+const expect = chai.expect;
+const tmp = temporaryDir();
+
+describe('CLI option file', () => {
+    let stdoutString = undefined;
+    let clockInterfaceFile;
+    let searchFuncFile;
+
+    const distFolder = 'test-config-file'; // Match /test/src/todomvc-ng2/.compodocrc
+
+    before(done => {
+        tmp.create(distFolder);
+
+        let ls = shell('node', [
+            './bin/index-cli.js',
+            '-c',
+            './test/src/todomvc-ng2/.compodocrc',
+            '-p',
+            './test/src/todomvc-ng2/src/tsconfig.json'
+        ]);
+
+        if (ls.stderr.toString() !== '') {
+            console.error(`shell error: ${ls.stderr.toString()}`);
+            done('error');
+        }
+        stdoutString = ls.stdout.toString();
+
+        done();
+    });
+    after(() => { tmp.clean(distFolder); });
+
+    it('should display generated message', () => {
+        expect(stdoutString).to.contain('Documentation generated');
+    });
+
+    it('should have generated main folder', () => {
+        const isFolderExists = exists(distFolder);
+        expect(isFolderExists).to.be.true;
+    });
+
+    it('should have generated main pages', () => {
+        const isIndexExists = exists(distFolder + '/index.html');
+        expect(isIndexExists).to.be.true;
+        const isModulesExists = exists(distFolder + '/modules.html');
+        expect(isModulesExists).to.be.true;
+        const isRoutesExists = exists(distFolder + '/routes.html');
+        expect(isRoutesExists).to.be.true;
+    });
+});
