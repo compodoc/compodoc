97952c5f65ebe01226c48c7c2565895776295210
Zach Feldman <Zachary-nathan.feldman@capitalone.com>
2019-02-11 18:45:37 -0500

    fix(CI): adding test for no custom logo, adding logo.svg to test images

--- test/src/cli/cli-custom-logo.spec.ts
+++ test/src/cli/cli-custom-logo.spec.ts
@@ -64,4 +64,28 @@ describe('CLI custom logo', () => {
             expect(originalFileSize).to.equal(copiedFileSize);
         });
     });
+
+    describe('when not specifying a custom logo svg image', () => {
+        before(function (done) {
+            tmp.create(distFolder);
+            let ls = shell('node', [
+                './bin/index-cli.js',
+                '-p', './test/src/todomvc-ng2/src/tsconfig.json',
+                '-d', distFolder
+            ]);
+
+            if (ls.stderr.toString() !== '') {
+                console.error(`shell error: ${ls.stderr.toString()}`);
+                done('error');
+            }
+
+            done();
+        });
+        after(() => tmp.clean(distFolder));
+
+        it('should not have copied the customLogo', () => {
+            let isFileExists = exists(`${distFolder}/images/logo.svg`);
+            expect(isFileExists).to.not.be.true;
+        });
+    });
 });
