65d5621a8b5e81f53bf0595a3ae34c4fb4fc2b9e
Nagy Gábor <gabor.nagy@icellmobilsoft.hu>
2019-06-20 11:03:40 +0200

    feat(i18n): add hungarian language

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,6 +1,7 @@
 export { TRANSLATION_EN_US } from './en-US';
 export { TRANSLATION_ES_ES } from './es-ES';
 export { TRANSLATION_FR_FR } from './fr-FR';
+export { TRANSLATION_HU_HU } from './hu-HU';
 export { TRANSLATION_IT_IT } from './it-IT';
 export { TRANSLATION_JA_JP } from './ja-JP';
 export { TRANSLATION_PT_BR } from './pt-BR';
