a8b51af2ee2f6bf0ac04626fdb63324d4cf6e093
Aristeidis Bampakos <bampakoa@gmail.com>
2020-01-31 17:28:39 +0200

    fix: use normalized paths in unit test coverage
    Convert filenames from the coverage summary json file using the `normalize` method of the `path` module to account for Windows machines.

--- src/app/application.ts
+++ src/app/application.ts
@@ -2197,7 +2197,7 @@ at least one config for the 'info' or 'source' tab in --navTabConfig.`);
             } else {
                 covDat = {};
                 covFileNames = _.map(coverageData.files, el => {
-                    let fileName = el.filePath;
+                    let fileName = path.normalize(el.filePath);
                     covDat[fileName] = {
                         type: el.type,
                         linktype: el.linktype,
@@ -2238,7 +2238,10 @@ at least one config for the 'info' or 'source' tab in --navTabConfig.`);
                         out = { name: fileName, filePath: fileName };
                     } else {
                         let findMatch = _.filter(covFileNames, el => {
-                            return el.includes(fileName) || fileName.includes(el);
+                            const normalizedFilename = path.normalize(fileName);
+                            return (
+                                el.includes(normalizedFilename) || normalizedFilename.includes(el)
+                            );
                         });
                         if (findMatch.length > 0) {
                             out = _.clone(covDat[findMatch[0]]);
