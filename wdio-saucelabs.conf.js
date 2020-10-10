ebbf9e52e80b04a2ea44bcd33f9f5f85659c683a
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-10-10 23:18:16 +0200

    feat(tool): e2e use chrome

--- wdio-saucelabs.conf.js
+++ wdio-saucelabs.conf.js
@@ -59,21 +59,7 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [
-        {
-            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-            // grid with only 5 firefox instances available you can make sure that not more than
-            // 5 instances get started at a time.
-            maxInstances: 5,
-            //
-            browserName: 'chrome',
-            acceptInsecureCerts: true,
-            // If outputDir is provided WebdriverIO can capture driver session logs
-            // it is possible to configure which logTypes to include/exclude.
-            // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-            // excludeDriverLogs: ['bugreport', 'server'],
-        },
-    ],
+    capabilities: [{ browserName: 'chrome', platform: 'Windows 10', version: 'latest' }],
     //
     // ===================
     // Test Configurations
