bed4d2beb818f80d9d34b4130c2a601a9fe0e148
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-10-11 08:29:35 +0200

    fix(tests): e2e with WebdriverIO / firefox

--- test/wdio/tests.js
+++ test/wdio/tests.js
@@ -1,12 +1,9 @@
 describe('Compodoc page', () => {
     it('should have a search bar, and handle results', async () => {
-        await browser.url('http://localhost:4000/components/FooComponent.html');
-        await browser.pause(2000);
+        await browser.url('http://localhost:4000/?q=exampleInput');
+        await browser.pause(1000);
         const searchBoxes = await $$("//*[@id='book-search-input']/input");
         const searchBox = searchBoxes[1];
-        await searchBox.click();
-        await searchBox.setValue('exampleInput');
-        await browser.pause(2000);
         const searchValue = await searchBox.getValue();
         await expect(searchValue).toMatch('exampleInput');
         const searchResults = await $$('.search-results-item');
@@ -14,13 +11,10 @@ describe('Compodoc page', () => {
     });
 
     it('should have a search bar, and handle results empty', async () => {
-        await browser.url('http://localhost:4000/components/FooComponent.html');
+        await browser.url('http://localhost:4000/?q=waza');
         await browser.pause(2000);
         const searchBoxes = await $$("//*[@id='book-search-input']/input");
         const searchBox = searchBoxes[1];
-        await searchBox.click();
-        await searchBox.setValue('waza');
-        await browser.pause(2000);
         const searchValue = await searchBox.getValue();
         await expect(searchValue).toMatch('waza');
         const searchResults = await $$('.search-results-item');
