18be3504a858b7d45f11e2106b6cda224e6925e8
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2021-02-15 16:03:42 +0100

    feat(e2e): increase timeout

--- test/wdio/tests.js
+++ test/wdio/tests.js
@@ -1,14 +1,14 @@
 describe('Compodoc page', () => {
     it('should have a search bar, and handle results', async () => {
         await browser.url('http://localhost:4000/?q=exampleInput');
-        await browser.pause(2000);
+        await browser.pause(3000);
         const searchResults = await $$('.search-results-item');
         await expect(searchResults.length).toEqual(1);
     });
 
     it('should have a search bar, and handle results empty', async () => {
         await browser.url('http://localhost:4000/?q=waza');
-        await browser.pause(2000);
+        await browser.pause(3000);
         const searchResults = await $$('.search-results-item');
         await expect(searchResults.length).toEqual(0);
     });
