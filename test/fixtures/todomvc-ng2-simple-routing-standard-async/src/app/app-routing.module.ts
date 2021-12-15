cff2ca5e903b54d08ff10807c6a275090c54598d
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2021-12-15 16:06:42 +0100

    fix(app): support for backtick in Routing lazy-loading syntax fix #1164

--- test/fixtures/todomvc-ng2-simple-routing-standard-async/src/app/app-routing.module.ts
+++ test/fixtures/todomvc-ng2-simple-routing-standard-async/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 export const APP_ROUTES: Routes = [
     { path: 'about', loadChildren: async () => (await import('./about/about.module')).AboutModule },
+    { path: 'toto', loadChildren: async () => (await import(`./toto/toto.module`)).TotoModule },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
