3afb3b315bddcbc5f38c1b76cfe5dd3892834ec3
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2024-03-14 16:04:09 +0100

    fix(app): tests with commander program init

--- src-refactored/core/use-cases/setup-flags.ts
+++ src-refactored/core/use-cases/setup-flags.ts
@@ -1,3 +1,5 @@
+const { program } = require('commander');
+
 import { Flag, PUBLIC_FLAGS } from '../entities/public-flags';
 import { CLIProgram } from '../entities/cli-program';
 
@@ -8,7 +10,7 @@ export class SetupFlags {
     public programOptions;
 
     constructor() {
-        this.program = require('commander');
+        this.program = program;
     }
 
     public static getInstance() {
